b0f381b3d258fcab52e86b333a96b22cb5513c63
Edmund Xin <mxinburritos@gmail.com>
2021-02-20 00:32:21 -0800

    add eslint

--- server/index.ts
+++ server/index.ts
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT, () => {
-	console.log(`Server started at port ${PORT}`);
+    console.log(`Server started at port ${PORT}`);
 });
